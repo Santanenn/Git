a83701bfd9f49aed77306aaf136522ba8014bbb0
Eveliina <evesantanen@gmail.com>
2024-10-25 18:44:36 +0300

    Itse peli
    Arvaa nimi, peli loppuu kun arvaat oikein.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        String input;
+        String oikeaNimi = ("Kalle");
+
+        do{
+            System.out.println("Arvaa nimi.");
+            input = in.nextLine();
+
+            if(input.equals(oikeaNimi))
+            {
+                System.out.println("Oikein!");
+                break;
+            }
+        }
+        while(true);
     }
 }

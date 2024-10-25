525ce85f29639146f451c1fe82833b02fe3e20b0
Eveliina <evesantanen@gmail.com>
2024-10-25 18:53:05 +0300

    Lopetus
    "Lopeta" lopettaa pelin

--- src/App.java
+++ src/App.java
@@ -7,13 +7,18 @@ public class App {
         int arvaukset = 0;
 
         do{
-            System.out.println("Arvaa nimi.");
+            System.out.println("Arvaa nimi. Kirjoita Lopeta lopettaaksesi");
             input = in.nextLine();
             arvaukset++;
 
             if(input.equals(oikeaNimi))
             {
                 System.out.println("Oikein! Arvasit " + arvaukset + " kertaa.");
+                break;
+            }
+            if(input.equals("Lopeta"))
+            {
+                System.out.println("Arvasit " + arvaukset + " kertaa.");
                 break;
             }
         }
